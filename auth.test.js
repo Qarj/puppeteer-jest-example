67d218bd81d30fc0ec1ac0a4e9b877f22df6c120
Tim Buckland <tim.buckland@gmail.com>
2022-03-30 21:47:36 +0100

    chore: change assertion

--- auth.test.js
+++ auth.test.js
@@ -35,7 +35,8 @@ describe("Search", () => {
         await Promise.all([page.click('input[id="btnLogin"]'), page.waitForNavigation()]);
 
         await page.goto(membersAreaUrl);
-        await expect(page).toMatch("Here is your job search at a glance", { timeout: 8000 });
+
+        await page.waitForFunction('document.querySelector("body").innerText.includes("Here is your job search at a glance")');
 
         // close session for next run
         await page.close();
