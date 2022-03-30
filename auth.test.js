52484edb06ee3454d8b54f5412d305a4c06a937a
Tim Buckland <tim.buckland@gmail.com>
2022-03-30 21:40:58 +0100

    chore: uncheck remember me

--- auth.test.js
+++ auth.test.js
@@ -3,40 +3,41 @@
 jest.setTimeout(45000);
 
 describe("Search", () => {
-  it("should enter login details", async () => {
-    // launch browser for LHCI
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080 });
-    const baseurl = "https://www.totaljobs.com";
-    const authurl = `${baseurl}/account/signin`;
-    const membersAreaUrl = `${baseurl}/membersarea`;
-    const email = "lighthouse@example.com";
-    const data2 = "ExamplePassword1";
-    await page.goto(authurl);
+    it("should enter login details", async () => {
+        // launch browser for LHCI
+        const page = await browser.newPage();
+        await page.setViewport({ width: 1920, height: 1080 });
+        const baseurl = "https://www.totaljobs.com";
+        const authurl = `${baseurl}/account/signin`;
+        const membersAreaUrl = `${baseurl}/membersarea`;
+        const email = "lighthouse@example.com";
+        const data2 = "ExamplePassword1";
+        await page.goto(authurl);
 
-    const cookies = [
-      {
-        name: "CONSENTMGR",
-        value: "consent:true",
-      },
-    ];
+        const cookies = [
+            {
+                name: "CONSENTMGR",
+                value: "consent:true",
+            },
+        ];
 
-    await page.setCookie(...cookies);
-    await page.goto(authurl);
+        await page.setCookie(...cookies);
+        await page.goto(authurl);
 
-    const emailInput = await page.$('input[id="Form_Email"]');
-    const data2Input = await page.$('input[id="Form_Password"]');
-    await emailInput.type(email);
-    await data2Input.type(data2);
+        const emailInput = await page.$('input[id="Form_Email"]');
+        const data2Input = await page.$('input[id="Form_Password"]');
+        await emailInput.type(email);
+        await data2Input.type(data2);
 
-    await Promise.all([
-      page.click('input[id="btnLogin"]'),
-      page.waitForNavigation(),
-    ]);
+        const checkboxInput = await page.$('input[id="Form_RememberMe"]');
+        await checkboxInput.click();
 
-    await page.goto(membersAreaUrl);
+        await Promise.all([page.click('input[id="btnLogin"]'), page.waitForNavigation()]);
 
-    // close session for next run
-    await page.close();
-  });
+        await page.goto(membersAreaUrl);
+        await expect(page).toMatch("Here is your job search at a glance", { timeout: 8000 });
+
+        // close session for next run
+        await page.close();
+    });
 });
